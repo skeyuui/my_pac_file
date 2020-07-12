c969e28267f36c1b71f0c6d281aa7b7fbf1b8eec
skeyuui <19658265+skeyuui@users.noreply.github.com>
2020-07-12 14:47:28 -0300

    Update pac.js

--- pac.js
+++ pac.js
@@ -1,7 +1,22 @@
 function FindProxyForURL(url, host) {
     // use proxy for specific domains
-    if (shExpMatch(url, "*.fc2.com*")){
-        alert(host);
+    if (shExpMatch(host, "*.fc2.com")){
+        alert(host + " foi por host = *.fc2.com");
+        return "PROXY 167.71.91.204:8080";
+    }
+    
+    if (shExpMatch(host, "fc2.com")){
+        alert(host + " foi por host = fc2.com");
+        return "PROXY 167.71.91.204:8080";
+    }
+    
+    if (shExpMatch(host, "*fc2.com*")){
+        alert(host + " foi por host = *fc2.com*");
+        return "PROXY 167.71.91.204:8080";
+    }
+    
+    if (shExpMatch(host, "*fc2*")){
+        alert(host + " foi por host = *fc2");
         return "PROXY 167.71.91.204:8080";
     }
     // by default use no proxy 
